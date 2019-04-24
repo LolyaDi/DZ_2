2ca4ac6d0337dd0810d026ce0d5a4fbc291fe21c
LolyaDi <winx55@list.ru>
2019-04-24 08:28:11 +0600

    Created

---
+++ DZ_2/DataAccessLayer.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.Common;
+using System.Configuration;
+using System;
+
+namespace DZ_2
+{
+    public class DataAccessLayer
+    {
+        public readonly string _connectionString;
+        public readonly string _providerName;
+        public readonly DbProviderFactory _providerFactory;
+        
+        public DataAccessLayer()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["appConnectionString"].ConnectionString;
+            _providerName = ConfigurationManager.ConnectionStrings["appConnectionString"].ProviderName;
+            _providerFactory = DbProviderFactories.GetFactory(_providerName);
+        }
+
+        public string CreateGroupsTable()
+        {
+            string message = "";
+
+            using (var connection = _providerFactory.CreateConnection())
+            using (var command = connection.CreateCommand())
+            {
+                try
+                {
+                    connection.ConnectionString = _connectionString;
+                    connection.Open();
+                    
+                    command.CommandText = "create table Groups ( Id int primary key identity, Name nvarchar(30) not null unique )";
+                    
+                    command.ExecuteNonQuery();
+
+                    message += "Таблица была успешно создана!";
+                }
+                catch (DbException)
+                {
+                    message += "Такая таблица уже существует";
+                }
+                catch(Exception)
+                {
+                    message += "Произошла ошибка!";
+                }
+            }
+
+            return message;
+        }
+    }
+}
---
+++ DZ_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DZ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataAccessLayer = new DataAccessLayer();
+
+            string result = dataAccessLayer.CreateGroupsTable();
+
+            System.Console.WriteLine(result);
+            System.Console.ReadLine();
+        }
+    }
+}
